72509b4cdb482ebc60b17c0595da5ce0ba14ff3c
Rafael <kubr2017@gmail.com>
2018-06-26 15:12:06 -0400

    added creating table.

--- designs.js
+++ designs.js
@@ -2,11 +2,22 @@
 // Select size input
 
 function buildGrid(){
-//   alert("Hello!");
 
-var gridHeight = document.getElementById("inputHeight");
-var gridWidth = document.getElementById("inputWidth");
-alert("Height:"+gridHeight.value+" Width:"+gridWidth.value);
+  var gridHeight = document.getElementById("inputHeight");
+  var gridWidth = document.getElementById("inputWidth");
+  //alert("Height:"+gridHeight.value+" Width:"+gridWidth.value);
+  var tbl = document.createElement("table");
+  for (var j= 0; j<=gridWidth.value;j++){
+    var row = document.createElement("tr");
+    for (var i = 0; i<=grid.Height.value;i++){
+      var cell = document.createElement("td");
+      row.appendChild(cell);
+    }
+    tbl.appendChild(row);
+  }
+  var body = document.getElementsByTagName("body")[0];
+  body.appendChild(tbl);
+
 }
 
 // When size is submitted by the user, call makeGrid()
