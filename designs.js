2fc553cbcfaea0ac55f10cf26fe1a010b8eec508
Rafael <kubr2017@gmail.com>
2018-06-26 15:30:29 -0400

    added text in cells in cycle.

--- designs.js
+++ designs.js
@@ -7,15 +7,18 @@ function buildGrid(){
   var gridWidth = document.getElementById("inputWidth");
   //alert("Height:"+gridHeight.value+" Width:"+gridWidth.value);
   var tbl = document.getElementById("pixelCanvas");
-  for (var j= 0; j<=gridWidth.value;j++){
+  for (var j= 1; j<=gridWidth.value;j++){
     var row = document.createElement("tr");
-    for (var i = 0; i<=gridHeight.value;i++){
+    for (var i = 1; i<=gridHeight.value;i++){
       var cell = document.createElement("td");
+      var cellText = document.createTextNode("cell is row "+j+", column "+i);
+
+                cell.appendChild(cellText);
       row.appendChild(cell);
     }
     tbl.appendChild(row);
   }
-  
+
 
 }
 
